9c16de5100f44a0b4811ad0a601b225daee129ea
puroc <p00u00r00>
2018-03-19 20:13:06 +0800

    1

--- config/index.js
+++ config/index.js
@@ -14,9 +14,9 @@ module.exports = {
       '/api': {
         // 测试环境
         target: 'http://localhost:8763',  // 接口域名
-        changeOrigin: true, //是否跨域
+        changeOrigin: true,  //是否跨域
         pathRewrite: {
-            '^/api': ''   //需要rewrite重写的,去掉path中的api
+            '^/api': ''   //需要rewrite重写的,
         }              
     }
     },
