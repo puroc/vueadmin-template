457bd6704583b4117de7cf687d54899001cbd9b0
puroc <p00u00r00>
2018-03-24 15:09:37 +0800

    1

--- src/api/user.js
+++ src/api/user.js
@@ -12,14 +12,35 @@ export function editUserByUsername(user) {
     url: '/api/idm/user/' + user.username,
     method: 'put',
     data: {
-      'payloads': [
+      payloads: [
         {
-          'name': user.name,
-          'sex': user.sex,
-          'birthday': user.birthday,
-          'phone': user.phone,
-          'email': user.email,
-          'orgId': user.orgId
+          name: user.name,
+          sex: user.sex,
+          birthday: user.birthday,
+          phone: user.phone,
+          email: user.email,
+          orgId: user.orgId
+        }
+      ]
+    }
+  })
+}
+
+export function _addUser(user) {
+  return request({
+    url: '/api/idm/user',
+    method: 'post',
+    data: {
+      payloads: [
+        {
+          username: user.username,
+          name: user.name,
+          password: user.password,
+          sex: user.sex,
+          birthday: user.birthday,
+          phone: user.phone,
+          email: user.email,
+          orgId: user.orgId
         }
       ]
     }
--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
-    <el-row>
-      <el-col :span="24">
-        <el-button type="primary">添加</el-button>
-        <el-button type="primary">操作</el-button>
-      </el-col>
-    </el-row>
+    <div style="height:60px">
+      <div style="line-height:60px;margin-left:30px">
+        <el-row>
+          <el-col :span="24">
+            <el-button type="primary" size="medium" @click="openAddUserDialog">添加</el-button>
+          </el-col>
+        </el-row>
+      </div>
+    </div>
     <el-table ref="multipleTable" border stripe highlight-current-row :data="users" tooltip-effect="dark" style="width: 100%" max-height="600" @selection-change="handleSelectionChange">
       <el-table-column type="selection">
       </el-table-column>
@@ -21,7 +24,7 @@
       </el-table-column>
       <el-table-column fixed="right" label="操作" width="100">
         <template slot-scope="scope">
-          <el-button @click="showUserDetails(scope.row)" type="text" size="small">查看</el-button>
+          <el-button @click="openEditUserDialog(scope.row)" type="text" size="small">查看</el-button>
           <el-button @click="deleteUser(scope.row)" type="text" size="small">删除</el-button>
         </template>
       </el-table-column>
@@ -33,55 +36,95 @@
       </div>
     </div>
     <!-- details dialog -->
-    <el-dialog title="用户详情" :visible.sync="dialogFormVisible" top="5vh">
+    <el-dialog title="用户详情" :visible.sync="editDialogFormVisible" top="5vh">
       <div>
         <el-button type="primary" icon="el-icon-edit" plain @click="switchToEdit" style="float:right;margin-right:5%"></el-button>
       </div>
       <br/>
       <!-- <div style="margin:5%; height:600px; overflow:auto"> -->
       <div style="margin:5%;">
-        <el-form :model="selectedUserForm" :rules="validateUserRules" ref="selectedUserForm">
+        <el-form :model="editUserForm" :rules="validateUserRules" ref="editUserForm">
           <el-form-item label="用户名" :label-width="formLabelWidth">
-            <el-input v-model="selectedUserForm.username" auto-complete="off" disabled></el-input>
+            <el-input v-model="editUserForm.username" auto-complete="off" disabled></el-input>
           </el-form-item>
           <el-form-item label="姓名" :label-width="formLabelWidth">
-            <el-input v-model="selectedUserForm.name" auto-complete="off" :disabled="editable"></el-input>
+            <el-input v-model="editUserForm.name" auto-complete="off" :disabled="editable"></el-input>
           </el-form-item>
           <el-form-item label="机构" :label-width="formLabelWidth">
-            <el-input v-model="selectedUserForm.orgId" auto-complete="off" :disabled="editable"></el-input>
+            <el-input v-model="editUserForm.orgId" auto-complete="off" :disabled="editable"></el-input>
           </el-form-item>
           <el-form-item label="性别" :label-width="formLabelWidth">
-            <el-input v-model="selectedUserForm.sex" auto-complete="off" :disabled="editable"></el-input>
+            <el-input v-model="editUserForm.sex" auto-complete="off" :disabled="editable"></el-input>
           </el-form-item>
           <el-form-item label="生日" :label-width="formLabelWidth">
-            <el-input v-model="selectedUserForm.birthday" auto-complete="off" :disabled="editable"></el-input>
+            <el-input v-model="editUserForm.birthday" auto-complete="off" :disabled="editable"></el-input>
           </el-form-item>
           <el-form-item label="手机" :label-width="formLabelWidth">
-            <el-input v-model="selectedUserForm.phone" auto-complete="off" :disabled="editable"></el-input>
+            <el-input v-model="editUserForm.phone" auto-complete="off" :disabled="editable"></el-input>
           </el-form-item>
           <el-form-item label="邮箱" :label-width="formLabelWidth">
-            <el-input v-model="selectedUserForm.email" auto-complete="off" :disabled="editable"></el-input>
+            <el-input v-model="editUserForm.email" auto-complete="off" :disabled="editable"></el-input>
           </el-form-item>
         </el-form>
       </div>
       <div slot="footer" class="dialog-footer">
-        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button @click="editDialogFormVisible = false">取 消</el-button>
         <el-button type="primary" @click="editUser">确 定</el-button>
       </div>
     </el-dialog>
+
+    <!-- add dialog -->
+    <el-dialog title="添加用户" :visible.sync="addDialogFormVisible" top="5vh">
+      <br/>
+      <div>
+        <el-form :model="addUserForm" :rules="validateUserRules" ref="addUserForm">
+          <el-form-item label="用户名" :label-width="formLabelWidth" prop='username'>
+            <el-input v-model="addUserForm.username" auto-complete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="密码" :label-width="formLabelWidth" prop='password'>
+            <el-input v-model="addUserForm.password" auto-complete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="姓名" :label-width="formLabelWidth" prop='name'>
+            <el-input v-model="addUserForm.name" auto-complete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="机构" :label-width="formLabelWidth" prop='orgId'>
+            <el-input v-model="addUserForm.orgId" auto-complete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="性别" :label-width="formLabelWidth" prop='sex'>
+            <el-input v-model="addUserForm.sex" auto-complete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="生日" :label-width="formLabelWidth" prop='birthday'>
+            <el-input v-model="addUserForm.birthday" auto-complete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="手机" :label-width="formLabelWidth" prop='phone'>
+            <el-input v-model="addUserForm.phone" auto-complete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="邮箱" :label-width="formLabelWidth" prop='email'>
+            <el-input v-model="addUserForm.email" auto-complete="off"></el-input>
+          </el-form-item>
+        </el-form>
+      </div>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="addDialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addUser">确 定</el-button>
+      </div>
+    </el-dialog>
+
   </div>
 </template>
 <script>
-import { getUserListByOrgId } from '@/api/org'
-import { deleteUserByUsername, editUserByUsername } from '@/api/user'
+import { getUserListByOrgId } from '@/api/org';
+import { deleteUserByUsername, editUserByUsername, _addUser } from '@/api/user';
 export default {
   data() {
     return {
       users: [],
       multipleSelection: [],
       currentPage: 1,
-      dialogFormVisible: false,
-      selectedUserForm: {},
+      editDialogFormVisible: false,
+      addDialogFormVisible: false,
+      editUserForm: {},
+      addUserForm: {},
       formLabelWidth: '70px',
       editable: true,
       validateUserRules: {
@@ -109,9 +152,9 @@ export default {
       this.editable = !this.editable
     },
     editUser() {
-      this.$refs.selectedUserForm.validate(valid => {
+      this.$refs.editUserForm.validate(valid => {
         if (valid) {
-          editUserByUsername(this.selectedUserForm)
+          editUserByUsername(this.editUserForm)
             .then(response => {
               if (response.data.resultCode === '1') {
                 this.$message({
@@ -121,7 +164,7 @@ export default {
                   center: true
                 })
               } else {
-                this.$refs.selectedUserForm.resetFields()
+                this.$refs.editUserForm.resetFields()
                 this.$message({
                   showClose: true,
                   message: '修改失败',
@@ -138,9 +181,46 @@ export default {
           return false
         }
       })
-      this.dialogFormVisible = false
+      this.editDialogFormVisible = false
     },
     deleteUser(row) {
+      this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      })
+        .then(() => {
+          deleteUserByUsername(row.username)
+            .then(response => {
+              if (response.data.resultCode === '1') {
+                this.$message({
+                  showClose: true,
+                  message: '删除成功',
+                  type: 'success',
+                  center: true
+                })
+              } else {
+                this.$message({
+                  showClose: true,
+                  message: '删除失败',
+                  type: 'error',
+                  center: true
+                })
+              }
+              this.getUserList(1)
+            })
+            .catch(error => {
+              console.log(error)
+            })
+        })
+        .catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除'
+          })
+        })
+    },
+    delete(row) {
       deleteUserByUsername(row.username)
         .then(response => {
           if (response.data.resultCode === '1') {
@@ -164,10 +244,52 @@ export default {
           console.log(error)
         })
     },
-    showUserDetails(row) {
-      this.dialogFormVisible = true
-      this.selectedUserForm = row
+
+    addUser() {
+      this.$refs.addUserForm.validate(valid => {
+        if (valid) {
+          _addUser(this.addUserForm)
+            .then(response => {
+              if (response.data.resultCode === '1') {
+                this.$message({
+                  showClose: true,
+                  message: '添加成功',
+                  type: 'success',
+                  center: true
+                })
+                this.getUserList(1)
+              } else {
+                this.$message({
+                  showClose: true,
+                  message: '添加失败',
+                  type: 'error',
+                  center: true
+                })
+              }
+            })
+            .catch(error => {
+              console.log(error)
+            })
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+      this.addDialogFormVisible = false
+    },
+    openAddUserDialog() {
+      this.addDialogFormVisible = true
+      this.resetForm('addUserForm')
+    },
+    openEditUserDialog(row) {
+      this.editDialogFormVisible = true
+      this.editUserForm = row
       this.editable = true
+    },
+    resetForm(formName) {
+      if (this.$refs[formName]) {
+        this.$refs[formName].resetFields()
+      }
     },
     handleSelectionChange() {
       alert('handleSelectionChange')
